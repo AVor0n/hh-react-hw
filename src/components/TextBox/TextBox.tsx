c706d5d9d8a81efbbc25e2c4ee8a7d3a993399ae
Voronin Andrey <Andrey.Voronin@infotecs.ru>
2024-02-01 21:00:51 +0300

    feat: add textbox component

---
+++ src/components/TextBox/TextBox.tsx
@@ -0,0 +1,27 @@
+import { InputHTMLAttributes } from 'react';
+import * as styles from './TextBox.module.css';
+
+type OmittedProps = 'onChange' | 'className' | 'placeholder';
+
+interface TextboxProps extends Omit<InputHTMLAttributes<HTMLInputElement>, OmittedProps> {
+  /** Описание поля */
+  label: string;
+  /** Обработчик изменения значения */
+  onChange?: (value: string) => void;
+}
+
+export const TextBox = ({ label, onChange, ...props }: TextboxProps) => {
+  return (
+    <div className={styles.container}>
+      <label>
+        <input
+          className={styles.input}
+          placeholder={label}
+          onChange={(event) => onChange?.(event.target.value)}
+          {...props}
+        />
+        <div className={styles.label}>{label}</div>
+      </label>
+    </div>
+  );
+};
