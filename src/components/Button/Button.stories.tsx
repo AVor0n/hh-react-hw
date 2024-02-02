d7cd2c34ee25dee375f3531ea700924e9610abb8
Andrey Voronin <AVor0n@yandex.ru>
2024-02-02 11:17:59 +0300

    feat: add button component

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from './Button';
+
+const meta = {
+  component: Button,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    children: 'text',
+    buttonType: 'primary',
+    size: 'm',
+  },
+};
