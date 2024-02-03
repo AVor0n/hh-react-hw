ed344b50312c51388fe079bb9d8530efbfd1ef27
AVor0n <79350701+AVor0n@users.noreply.github.com>
2024-02-03 00:54:02 +0000

    Deploying to gh-pages from @ AVor0n/hh-react-hw@7a7273e4460cf4a1acee857d06cd58685b56a34a 🚀

---
+++ docs/assets/preview-L0Ckvmw2.js
@@ -0,0 +1,7 @@
+import{_ as t}from"./iframe-BMEvq1_M.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-u95K8HBP.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7]));return new r}}};export{_ as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/DocsRenderer-NNNQARDV-u95K8HBP.js","assets/iframe-BMEvq1_M.js","assets/index-4g5l5LRQ.js","assets/_commonjsHelpers-4gQjN7DL.js","assets/react-18-0wh8ubQX.js","assets/index-ogXoivrg.js","assets/_getPrototype-bjD8Yebc.js","assets/index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
